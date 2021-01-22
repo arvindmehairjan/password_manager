b3d470384c71f6d5f4d2d08e71dac12f39046479
despi <despi@LAPTOP-JL8GAFLU>
2021-01-22 19:30:33 +0100

    Added save functionality

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+
+    website = enter_field_website.get()
+    email = enter_field_email.get()
+    password = enter_password.get()
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password} \n")
+        enter_field_website.delete(0, END)
+        enter_password.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -22,15 +33,17 @@ password_label.grid(column=0, row=3)
 
 enter_field_website = Entry(width=35)
 enter_field_website.grid(column=1, row=1, columnspan=2)
+enter_field_website.focus()
 enter_field_email = Entry(width=35)
 enter_field_email.grid(column=1, row=2, columnspan=2)
+enter_field_email.insert(END, "example@email.com")
 enter_password = Entry(width=21)
 enter_password.grid(column=1, row=3)
 
 generate_password = Button(text="Generate Password")
 generate_password.grid(column=2, row=3)
 
-submit_button = Button(text="Add", width=36)
+submit_button = Button(text="Add", width=36, command=save)
 submit_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
